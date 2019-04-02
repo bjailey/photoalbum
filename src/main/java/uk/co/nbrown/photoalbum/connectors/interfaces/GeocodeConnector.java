256c55da5530b6900d002189215ea32491a7e627
Jake Bailey <jakebailey93@outlook.com>
2019-04-02 22:33:17 +0100

    directory rename

--- src/main/java/uk/co/nbrown/photoalbum/connectors/interfaces/GeocodeConnector.java
+++
@@ -1,7 +0,0 @@
-package uk.co.nbrown.photoalbum.connectors.interfaces;
-
-public interface GeocodeConnector {
-
-	String getLocation(String geocode);
-	
-}
--- src/main/java/uk/co/nbrown/photoalbum/exceptions/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package uk.co.nbrown.photoalbum.exceptions;
-
-public class InternalServerErrorException extends RuntimeException {
-
-	private static final long serialVersionUID = 391171821305364741L;
-
-}
